deff2c6a25f4c145c92089da43289ad8cedc5ddb
martijn van de ree <mrtjnvanderee@gmail.com>
2020-11-04 18:18:43 +0100

    added blog

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -8,42 +8,33 @@ type Props = {
   blogPosts: any[];
 };
 
-// const Blog: React.FunctionComponent<Props> = ({ blogPosts }) => {
-//   return (
-//     <div className="relative">
-//       <div className="overlay-background-image bg-blue-100 opacity-25" />
-//       <Nav />
-
-//       <div className="content-container">
-//         <Title
-//           color="#013f5d"
-//           title="Blog"
-//           subtitle="Lees hier over mijn zoektocht naar beter onderwijs"
-//         />
-
-//         {/* <div className="grid md:grid-cols-3 gap-4 max-w-6xl mx-auto ">
-//           {blogPosts.map((blogPost, index) => (
-//             <BlogPost blogPost={blogPost} key={index} />
-//           ))}
-//         </div> */}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export async function getStaticProps() {
-//   const data = await fetcher([process.env.API_URL, "posts"]);
-
-//   return { props: { blogPosts: data.data } };
-//}
-
-
 const Blog: React.FunctionComponent<Props> = ({ blogPosts }) => {
   return (
     <div className="relative">
-    blog
+      <div className="overlay-background-image bg-blue-100 opacity-25" />
+      <Nav />
+
+      <div className="content-container">
+        <Title
+          color="#013f5d"
+          title="Blog"
+          subtitle="Lees hier over mijn zoektocht naar beter onderwijs"
+        />
+
+        {/* <div className="grid md:grid-cols-3 gap-4 max-w-6xl mx-auto ">
+          {blogPosts.map((blogPost, index) => (
+            <BlogPost blogPost={blogPost} key={index} />
+          ))}
+        </div> */}
+      </div>
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const data = await fetcher([process.env.API_URL, "posts"]);
+
+  return { props: { blogPosts: data.data } };
+}
 
 export default Blog;
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,39 +9,32 @@ import { BackgroundImage } from "../components/backgroundImage";
 
 import { fetcher } from "../fetcher";
 
-// const Home = ({ blogPosts }: any) => (
-//   <div className="relative h-screen v-screen">
-//     <Nav />
-//     <BackgroundImage />
-//     <main className="content-container">
-//       <Title
-//         color="white"
-//         title="Dennis Stassen"
-//         subtitle="Enthousiast Econoom & Leergierige Leraar"
-//       />
-//       <>
-//         <Header />
-//         {/* <BlogPosts blogPosts={blogPosts} /> */}
-//       </>
-//     </main>
-//   </div>
-// );
-
-// export async function getStaticProps() {
-//   const data = await fetcher([process.env.API_URL, "posts"]);
-
-//   const first3Data = R.slice(0, 3, data.data);
-
-//   return { props: { blogPosts: first3Data } };
-// }
-
-
-const Home = () => (
+const Home = ({ blogPosts }: any) => (
   <div className="relative h-screen v-screen">
-  home
+    <Nav />
+    <BackgroundImage />
+    <main className="content-container">
+      <Title
+        color="white"
+        title="Dennis Stassen"
+        subtitle="Enthousiast Econoom & Leergierige Leraar"
+      />
+      <>
+        <Header />
+        {/* <BlogPosts blogPosts={blogPosts} /> */}
+      </>
+    </main>
   </div>
 );
 
 
+
 export default Home;
 
+export async function getStaticProps() {
+  const data = await fetcher([process.env.API_URL, "posts"]);
+
+  const first3Data = R.slice(0, 3, data.data);
+
+  return { props: { blogPosts: first3Data } };
+}
